015f2e3be6a95cad46da7b1cf04ffcf89a4b497f
Ji Zhou <calf.zhou@gmail.com>
2022-07-21 10:57:40 +0800

    feat(captcha/demo): add demo captcha class

---
+++ captcha/demo.py
@@ -0,0 +1,39 @@
+
+
+from .base import BaseCaptcha
+
+
+class DemoCaptcha(BaseCaptcha):
+    white_chars = '02468BDFHJLNPRTVXZ'
+    charset = set(white_chars)
+
+    @classmethod
+    @property
+    def tesseract_config_array(cls):
+        config = super().tesseract_config_array
+        config = [
+            *config,
+            '-c', f'tessedit_char_whitelist={cls.white_chars}',
+        ]
+        return config
+
+    def __init__(self):
+        super().__init__()
+        self.preview_scale = 5
+        self.tesseract_lang = 'eng_fast'
+        self.image_ext = '.gif'
+
+    def preprocess(self, image_fp):
+        image = super().preprocess(image_fp)
+
+        # TODO: Clean image.
+
+        return image
+
+    def validate_captcha_input(self, captcha):
+        if len(captcha) != 5:
+            raise ValueError('number of characters is not 5')
+
+        invalid_chars = set(captcha) - self.charset
+        if invalid_chars:
+            raise ValueError(f'contains invalid chars {invalid_chars}')
