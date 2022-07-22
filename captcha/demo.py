cb1f9ced4a9af81c002e89bb5f4bc416455560ad
Ji Zhou <calf.zhou@gmail.com>
2022-07-22 22:28:41 +0800

    feat(captcha/demo): add image clean code, re-train, correctness 99.2%

--- captcha/demo.py
+++ captcha/demo.py
@@ -1,4 +1,8 @@
 
+import colorsys
+from itertools import product
+
+from PIL import Image
 
 from .base import BaseCaptcha
 
@@ -24,8 +28,40 @@ class DemoCaptcha(BaseCaptcha):
 
     def preprocess(self, image_fp):
         image = super().preprocess(image_fp)
+        width = image.width
+        height = image.height
+        index = lambda x, y: y * width + x
 
-        # TODO: Clean image.
+        # Binaryzation.
+        image = image.convert('RGB')
+        bin_data = [1] * (width * height)
+        max_l = 0.6
+        min_s = 0.65
+        for (y, x), (r, g, b) in zip(product(range(height), range(width)), image.getdata()):
+            _, l, s = colorsys.rgb_to_hls(r / 255, g / 255, b / 255)
+            if 0 < x < width - 1 and 0 < y < height - 1 and l <= max_l and s >= min_s:
+                bin_data[index(x, y)] = 0
+
+        # Remove random dots.
+        # Left, right, up, down.
+        straight_offsets = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        # Up-right, up-left, down-left, down-right.
+        diagonal_offsets = [(1, -1), (-1, -1), (-1, 1), (1, 1)]
+        clean_data = list(bin_data)
+        for y, x in product(range(1, height - 1), range(1, width - 1)):
+            center_index = index(x, y)
+            center_value = bin_data[center_index]
+            straight_sum = sum(bin_data[index(x + h, y + v)] for h, v in straight_offsets)
+            diagonal_sum = sum(bin_data[index(x + h, y + v)] for h, v in diagonal_offsets)
+            if center_value == 1:
+                if straight_sum == 0 and diagonal_sum <= 1:
+                    clean_data[center_index] = 0
+            elif center_value == 0:
+                if straight_sum == 4 or (diagonal_sum == 4 and straight_sum >= 2):
+                    clean_data[center_index] = 1
+
+        image = Image.new('1', image.size)
+        image.putdata(clean_data)
 
         return image
 
